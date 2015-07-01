cb6d3a111a471e432e04103209abba9715973990
Dan <cobbdb@gmail.com>
2015-07-01 14:10:45 -0700

    .

--- src/assets/font.js
+++ src/assets/font.js
@@ -1,5 +1,13 @@
 var Str = require('curb');
 
+// Create invisible <span> to force font prefetch.
+function prefetch(family) {
+    var span = global.document.createElement('span');
+    span.style.fontFamily = family;
+    span.style.display = 'none';
+    global.document.body.appendChild(span);
+}
+
 /**
  * @param {String} family
  * @param {String} opts.src
@@ -9,6 +17,7 @@ var Str = require('curb');
  * @return {Boolean} Always returns true.
  */
 module.exports = function (family, opts, onload) {
+    prefetch(family);
     var style = global.document.createElement('style');
     //new Image().src = 'assets/font/' + opts.src;
     style.innerHTML = Str('@font-face{%s}', [
